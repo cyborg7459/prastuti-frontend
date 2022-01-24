f9304f801ab61ad95933c7e8a0188237dfd79d91
Shreyash Singh <shreyashsingh17@gmail.com>
2022-01-24 18:17:13 +0530

    .

--- src/pages/Profilepage/profilePage.jsx
+++ src/pages/Profilepage/profilePage.jsx
@@ -100,6 +100,7 @@ const ProfilePage = () => {
             })
             .then(res => {
                 setShowLoader(false);
+                localStorage.setItem('prastuti-user', JSON.stringify(res.data[0]));
                 setUser(res.data[0]);
             })
             .catch(err => {
