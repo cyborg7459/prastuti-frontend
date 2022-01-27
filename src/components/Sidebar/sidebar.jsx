e645c2ccf15a668e9d39e6cf29e7e2022648ce99
Shreyash Singh <shreyashsingh17@gmail.com>
2022-01-28 03:52:46 +0530

    .

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { MdOutlineEmojiEvents } from 'react-icons/md'
 import { CgProfile } from 'react-icons/cg'
 import { FiLogOut } from 'react-icons/fi'
+import { AiOutlineHome } from 'react-icons/ai'
 
 import './sidebarStyle.scss';
 
@@ -15,6 +16,7 @@ const Sidebar = () => {
     return (
         <div id="sidebar">
             <Link to='/'><h1 className='shadow-heading'>Prastuti '22</h1></Link>
+            <Link to='/'><div className="option option-event"><AiOutlineHome className='icon'/>Home</div></Link>
             <Link to='/events'><div className="option option-event"><MdOutlineEmojiEvents className='icon'/>Events</div></Link>
             <Link to='/profile'><div className="option option-profile"><CgProfile className='icon'/>Profile</div></Link>
             <div onClick={Logout} className="option"> <FiLogOut className='icon'/> Sign Out</div>
